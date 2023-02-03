a5d7cf68c649069d39ddf8dabd6190ab0d6e30fa
ArturBerk <berkmail@mail.ru>
2023-02-04 00:11:57 +0600

    Two new loops added: Initialization and EarlyUpdate

--- Runtime/Loops.cs
+++ Runtime/Loops.cs
@@ -4,9 +4,11 @@ namespace Abg
 {
     public static class Loops
     {
+        public static readonly ILoopSystem Initialization = new LoopSystem<Initialization>();
+        public static readonly ILoopSystem EarlyUpdate = new LoopSystem<EarlyUpdate>();
+        public static readonly ILoopSystem FixedUpdate = new LoopSystem<FixedUpdate>();
         public static readonly ILoopSystem PreUpdate = new LoopSystem<PreUpdate>();
         public static readonly ILoopSystem Update = new LoopSystem<Update>();
-        public static readonly ILoopSystem FixedUpdate = new LoopSystem<FixedUpdate>();
         public static readonly ILoopSystem PreLateUpdate = new LoopSystem<PreLateUpdate>();
         public static readonly ILoopSystem PostLateUpdate = new LoopSystem<PostLateUpdate>();
     }
